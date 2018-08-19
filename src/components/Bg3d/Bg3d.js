6eaa686271e10e609179d913a3ae402508379773
rugia <rugia813@yahoo.com.tw>
2018-08-19 19:35:05 +0800

    intro animation

--- src/components/Bg3d/Bg3d.js
+++ src/components/Bg3d/Bg3d.js
@@ -4,6 +4,7 @@ import EffectComposer, { RenderPass, ShaderPass } from 'three-effectcomposer-es6
 import { BloomPass, FilmPass, GlitchPass } from "postprocessing";
 import { FocusShader, DotScreenShader, RGBShiftShader, OrbitControls } from 'three-addons';
 import { Stats } from 'three-stats';
+import TWEEN from "@tweenjs/tween.js";
 
 
 let camera, scene, renderer, mesh, renderTarget;
@@ -23,9 +24,9 @@ let stats;
 
 const particleControl = new function () {
     this.radius = 40;
-    this.tube = 28.2;
-    this.radialSegments = 600;
-    this.tubularSegments = 12;
+    this.tube = 9;
+    this.radialSegments = 200;
+    this.tubularSegments = 15;
     this.p = 5;
     this.q = 4;
     this.heightScale = 4;
@@ -35,8 +36,8 @@ const particleControl = new function () {
         // remove the old plane
         if (knot) scene.remove(knot);
         // create a new one
-        var geom = new THREE.TorusKnotGeometry(particleControl.radius, particleControl.tube, Math.round(particleControl.radialSegments), Math.round(particleControl.tubularSegments), Math.round(particleControl.p), Math.round(particleControl.q), particleControl.heightScale);
-
+        var geom = new THREE.TorusKnotGeometry(particleControl.radius, particleControl.tube, Math.round(particleControl.radialSegments), Math.round(particleControl.tubularSegments), Math.round(particleControl.p), Math.round(particleControl.q));
+        geom.scale(particleControl.heightScale, particleControl.heightScale, particleControl.heightScale)
         knot = createParticleSystem(geom);
 
         // add it to the scene.
@@ -60,6 +61,16 @@ export function init() {
     scene = new THREE.Scene();
 
     particleControl.redraw();
+
+    setTimeout(() => {
+        new TWEEN.Tween(particleControl)
+            .to({ radius: 1, heightScale: 1 }, 3000)
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .onUpdate(function() {
+                particleControl.redraw();
+            })
+            .start();
+    }, 500);
     
     renderer = new THREE.WebGLRenderer( { antialias: false, alpha: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -68,17 +79,17 @@ export function init() {
     
 }
     
-export function animate() {
+export function animate(time) {
     
     requestAnimationFrame( animate );
     
-    mesh && (mesh.rotation.y += 0.02);
+    // mesh && (mesh.rotation.y += 0.02);
     renderer.render( scene, camera );
-    var delta = clock.getDelta();
+    // var delta = clock.getDelta();
     // composer.render(delta)
-
+	TWEEN.update(time);
     if (particleControl.rotate) {
-        knot.rotation.y = step += 0.01;
+        knot.rotation.y = step += 0.005;
     }
 
 }
@@ -91,9 +102,9 @@ function generateSprite() {
 
     var context = canvas.getContext('2d');
     var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-    gradient.addColorStop(0, 'rgba(255,255,255,1)');
-    gradient.addColorStop(0.2, 'rgba(0,255,255,1)');
-    gradient.addColorStop(0.4, 'rgba(0,0,64,0.8)');
+    gradient.addColorStop(0, 'rgba(200,200,200,1)');
+    gradient.addColorStop(0.2, 'rgba(65,255,255,1)');
+    gradient.addColorStop(0.4, 'rgba(0,0,255,0.8)');
     gradient.addColorStop(1, 'rgba(0,0,0,0)');
 
     context.fillStyle = gradient;
@@ -108,8 +119,8 @@ function generateSprite() {
 function createParticleSystem(geom) {
     var material = new THREE.PointsMaterial({
         color: 0xffffff,
-        size: 3,
-        alphaTest: 0.5,
+        size: 2,
+        alphaTest: 0.8,
         transparent: true,
         blending: THREE.AdditiveBlending,
         map: generateSprite()
